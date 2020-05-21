ccd92d22db55ca5ca330e1509b0c8b2bfe9837e3
Stephen Renda <stephenrendajr@gmail.com>
2020-05-21 14:07:06 -0700

    test

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ const key = process.env.REACT_APP_SENDGRID;
 
 sendGrid.setApiKey(key);
 
-app.use(cors());
+console.log('GOT HERE!!');
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Change later to only allow our server
@@ -49,11 +50,14 @@ app.post('/api/email', (req, res, next) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static('client/build'));
 // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  // app.get('*', function(req, res) {
+  //   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  // });
+  app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 }
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
